6f93000ac916e61875f3d43689527a84b87969f3
BennoAlif <bennoalif41@gmail.com>
2019-09-25 17:03:30 +0700

    Initial commit

---
+++ src/com/benno/Main.java
@@ -0,0 +1,18 @@
+package com.benno;
+
+    /*
+    NAMA        : BENNO ALIF ANGAGARA
+    KELAS       : IF1
+    NIM         : 10118024
+    Deskripsi   : Mendeklarasikan dan implementasi operasi Logika
+     */
+
+public class Main {
+
+    public static void main(String[] args) {
+        int umur = 19;
+        boolean dewasa;
+        dewasa = (umur >= 17);
+        System.out.println("Status dewasa : " + dewasa);
+    }
+}
